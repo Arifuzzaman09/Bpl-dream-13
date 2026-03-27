d72981b67fe965232f731b83a35a34ccd8e8e876
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-03-27 11:07:04 +0600

    added toggle btn

--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -1,13 +1,28 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Avaliable from '../AvaliablesPlayers/Avaliable';
+import SelectedPlayers from '../SelectedPlayers/SelectedPlayers';
 
-const Players = ({playersPromise}) => {
+const Players = ({ playersPromise }) => {
     const players = use(playersPromise)
-    
+    const [selectedType, setSelectedType] = useState("available")
+
     return (
         <div className='container mx-auto'>
-            Players:{players.length}
-            <Avaliable players={players}></Avaliable>
+            <div className='flex justify-between items-center my-2'>
+                {selectedType === "available" ? 
+                (<h2 className='font-bold text-xl'>Available Players</h2>)
+                    : (<h2 className='font-bold text-xl'>Selected Players (2/8)</h2>)}
+                <div>
+                    <button
+                        onClick={() => setSelectedType("available")}
+                        className={`btn  ${selectedType === "available" ? "bg-[#E7FE29]" : ""} rounded-r-none rounded-l-xl `}>Available</button>
+                    <button
+                        onClick={() => setSelectedType("selected")}
+                        className={`btn  ${selectedType === "selected" ? "bg-[#E7FE29]" : ""} rounded-l-none rounded-r-xl`}>Selected (0)</button>
+                </div>
+            </div>
+           {selectedType === "available"? (<Avaliable players={players}></Avaliable>):
+           (<SelectedPlayers></SelectedPlayers>)}
         </div>
     );
 };
